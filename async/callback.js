ac47eed243813a34261ad8ccfcf781e0e5062770
Daniel Bravo <daniel.brv@hotmail.cl>
2021-08-26 17:55:19 -0400

    Se agrega clase completa de callbacks

--- async/callback.js
+++ async/callback.js
@@ -1,11 +1,23 @@
-function soyAsincrona(callback) {
-    console.log('Hola, soy una función asincrona');
+function hola(nombre,callback) {
     setTimeout(function(){
-        console.log('Estoy siendo asíncrona')
-        callback();
+        console.log('Hola '+ nombre);
+        callback(nombre);
     },2000)
 }
+
+function adios(nombre, otroCallback){
+    setTimeout(function(){
+        console.log('Adios '+ nombre);
+        otroCallback();
+    },1000);
+}
+
 console.log('Iniciando proceso...');
-soyAsincrona(function() {
+hola('Carlos',function(nombre) {
+    adios(nombre, function(){
     console.log('terminadndo proceso...');
+    })
 });
+
+//hola('Carlos',function(){});
+//adios('Carlos',function(){});
